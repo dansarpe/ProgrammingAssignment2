ea5a3bd5e56bd7c1664a196003ea176737c9f163
github <dansar@intarmedia.net>
2020-10-08 10:08:11 -0500

    makeCacheMatrix and cacheSolve for Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,33 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  
+  ## Inicializa la propiedad inversa
+  inv <- NULL
+  
+  ## Método para configurar la matriz
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  
+  ## Método para obtener el valor de la matriz
+  get <- function(){x}
+  
+  ## Método para establecer la inversa de la matriz
+  setInverse <- function(inverse){ inv <<-  inverse}
+  
+  ## Método para obtener la inversa de la matriz
+  getInverse <- function(){
+    ## Devuelve la propiedad inversa
+    inv
+  }
+  
+  ## Devuelve una lista de los métodos
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
 
@@ -12,4 +37,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  
+  if(!is.null (inv)){
+    message("Obteniendo datos del cache")
+    return(inv)
+  }
+  
+  ## Obtener la matriz de nuestro objeto
+  mat <- x$get()
+  
+  ## Calcula la inversa usando la multiplicación de matrices
+  inv <- solve(mat, ...)
+  
+  ## Establecer la inversa al objeto
+  x$setInverse(inv)
+  
+  ## Devuelve la matriz
+  inv
 }
